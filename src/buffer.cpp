fe23524725c9c97949282cd6c31d5df43b113e0e
twu287 <91846668+twu287@users.noreply.github.com>
2022-02-14 23:25:59 -0600

    Taiyen update buffer.cpp
    advanceClock(), unPinPage(), flushFile()

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -38,7 +38,9 @@ BufMgr::BufMgr(std::uint32_t bufs)
   clockHand = bufs - 1;
 }
 
-void BufMgr::advanceClock() {}
+void BufMgr::advanceClock() {
+  (clockHand++)%numBufs;
+}
 
 void BufMgr::allocBuf(FrameId &frame)
 {
@@ -80,7 +82,37 @@ void BufMgr::allocBuf(FrameId &frame)
 
 void BufMgr::readPage(File& file, const PageId pageNo, Page*& page) {}
 
-void BufMgr::unPinPage(File& file, const PageId pageNo, const bool dirty) {}
+void BufMgr::unPinPage(File& file, const PageId pageNo, const bool dirty) {
+
+        //should use hashtable to check?
+    bool exist = false;
+    int i;
+   
+    for (i = 0; i < bufPool.size(); i++) {
+      if(bufPool[i].page_number() == pageNo) {
+        exist = true;
+        break;
+      }
+    }
+
+    if (exist) 
+      throw PageNotPinnedException(bufDescTable[i].file.filename(), bufDescTable[i].pageNo, bufDescTable[i].frameNo); // const std::string &nameIn, PageId pageNoIn,FrameId frameNoIn
+
+  
+  if (bufDescTable[i].pinCnt > 0 && !dirty ) {
+    bufDescTable[i].pinCnt --;
+  }
+
+  else if (bufDescTable[i].pinCnt > 0 && dirty) {
+    bufDescTable[i].pinCnt --;
+    bufDescTable[i].dirty = false;
+  }
+
+  else {
+    throw PageNotPinnedException(bufDescTable[i].file.filename(), bufDescTable[i].pageNo, bufDescTable[i].frameNo);;
+  }
+    
+}
 
 void BufMgr::allocPage(File &file, PageId &pageNo, Page *&page)
 {
@@ -99,7 +131,35 @@ void BufMgr::allocPage(File &file, PageId &pageNo, Page *&page)
     
 }
 
-void BufMgr::flushFile(File& file) {}
+void BufMgr::flushFile(File& file) {
+      
+    int i;
+  
+  for (i = 0; i < bufPool.size(); i++) {
+    
+    if(bufDescTable[i].file.filename() == file.filename()) {
+
+      if(bufDescTable[i].pinCnt > 0) { //Throws PagePinnedException if some page of the file is pinned.
+        throw PagePinnedException(bufDescTable[i].file.filename(), bufDescTable[i].pageNo, bufDescTable[i].frameNo);
+      }
+
+        if(!bufDescTable[i].valid) { //Throws BadBufferException if an invalid page belonging to the file is encountered.
+        throw BadBufferException(bufDescTable[i].frameNo, bufDescTable[i].dirty, bufDescTable[i].valid, bufDescTable[i].refbit);
+      }
+      
+      if(bufDescTable[i].dirty) { //(a) if the page is dirty, call file.writePage() to flush the page to disk and then set the dirty bit for the page to false.
+        file.writePage(file.readPage(bufDescTable[i].pageNo));
+        //file.writePage(bufDescTable[i].pageNo, file.readPage(bufDescTable[i].pageNo));
+        bufDescTable[i].dirty = false;
+      } 
+
+      hashTable.remove(file, bufDescTable[i].pageNo); //(b) remove the page from the hashtable (whether the page is clean or dirty)
+
+      bufDescTable[i].clear(); //(c) invoke the Clear() method of BufDesc for the page frame.
+
+    }
+  }   
+}
 
 void BufMgr::disposePage(File& file, const PageId PageNo) {}
 
